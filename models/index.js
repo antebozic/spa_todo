c91bf5f938b359082a6604bf6d4dc89f06338ff8
ANTE BOZIC <antebozic@gmail.com>
2018-09-18 00:16:09 +0200

    heroku setup

--- models/index.js
+++ models/index.js
@@ -1,7 +1,12 @@
 var mongoose = require('mongoose');
 mongoose.set('debug', true);
-mongoose.connect('mongodb://localhost/spa-todo-api');
 
+if (process.env.NODE_ENV == "production") {
+    mongoose.connect(process.env.MLAB_URL)
+  } else {
+    mongoose.connect('mongodb://localhost/spa-todo-api');
+  }
+  
 mongoose.Promise = Promise;
 
 module.exports.Todo = require('./todo');
