58f84d51e09d5fb3f96d2d89ae490733f0b04ea2
ANTE BOZIC <antebozic@gmail.com>
2018-09-18 12:57:01 +0200

    updated heroku settings

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 mongoose.set('debug', true);
+process.env.MLAB_URL = "mongodb://<your_user_login>:<your_user_password>@ds015760.mlab.com:15760/<your_db_name>"
 
 if (process.env.NODE_ENV == "production") {
     mongoose.connect(process.env.MLAB_URL)
